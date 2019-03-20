2d5b31cd78f6591c1c11a8680f496e57487ac6de
clapertosa <the_wolf91@libero.it>
2019-03-20 16:40:11 +0100

    Folders refactoring

--- store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from "redux-thunk";
-
-const rootReducer = combineReducers({});
-
-export function initializeStore() {
-  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-}
