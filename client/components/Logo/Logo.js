2bfe6ab38886bb64e436ff4454ada199aa40033a
clapertosa <the_wolf91@libero.it>
2019-03-20 16:49:22 +0100

    Logo component

---
+++ client/components/Logo/Logo.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  grid-area: logo;
+  margin: auto;
+  max-width: 100px;
+  width: 100%;
+  height: auto;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+
+  @media (min-width: ${({ theme: { mediaQueryMinWidth } }) =>
+      mediaQueryMinWidth}) {
+    max-width: 200px;
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const Logo = () => {
+  return (
+    <Container>
+      <img
+        src="/static/quote.svg"
+        alt="Quotesland logo: colored apexes in a circle background"
+      />
+    </Container>
+  );
+};
+
+export default Logo;
