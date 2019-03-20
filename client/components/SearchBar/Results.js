7b158d4709e852f8ff2c86ed3948816913989fe4
clapertosa <the_wolf91@libero.it>
2019-03-20 16:50:00 +0100

    SearchBar and Results components

---
+++ client/components/SearchBar/Results.js
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: absolute;
+  top: 50px;
+  width: 100%;
+  max-height: 350px;
+  background-color: #ffb4a2;
+  border: 2px solid #6d6875;
+  border-radius: 15px;
+  overflow: auto;
+`;
+
+const List = styled.ul`
+  padding: 0;
+
+  li {
+    list-style-type: none;
+    padding: 4px 10px 4px 10px;
+    text-align: center;
+  }
+`;
+
+const ListItem = styled.li`
+  cursor: pointer;
+  list-style-type: none;
+  padding: 4px 10px 4px 10px;
+  text-align: center;
+  transition: background-color 0.3s;
+
+  &:hover,
+  &:focus {
+    background-color: #e5989b;
+  }
+`;
+
+const Results = ({ authorSelected, show, authors, query, loading }) => {
+  if (show && authors.length > 0 && query.trim().length > 0 && !loading) {
+    let filteredAuthors = authors.filter(author =>
+      author.name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    const select = (e, name, count) => {
+      const resultsLength = document.querySelectorAll("li[tabindex]").length;
+      if (e.which === 40) {
+        e.target.tabIndex < resultsLength - 1
+          ? document
+              .querySelector(`li[tabindex="${e.target.tabIndex + 1}"]`)
+              .focus()
+          : document.querySelector("li[tabindex='0']").focus();
+      } else if (e.which === 38) {
+        e.target.tabIndex > 0
+          ? document
+              .querySelector(`li[tabindex="${e.target.tabIndex - 1}"]`)
+              .focus()
+          : document
+              .querySelector(`li[tabindex='${resultsLength - 1}']`)
+              .focus();
+      } else if (e.which === 13) {
+        authorSelected(name, count);
+      }
+    };
+
+    return (
+      <Container>
+        <List>
+          {filteredAuthors.length > 0 ? (
+            filteredAuthors
+              .map((author, index) => (
+                <ListItem
+                  key={author.permalink}
+                  tabIndex={index}
+                  onClick={() => authorSelected(author.name, author.count)}
+                  onKeyDown={e => select(e, author.name, author.count)}
+                >
+                  {author.name} ({author.count} quotes)
+                </ListItem>
+              ))
+              .slice(0, 10)
+          ) : (
+            <li>Sorry, no results 🤕</li>
+          )}
+        </List>
+      </Container>
+    );
+  }
+
+  return null;
+};
+
+export default Results;
---
+++ client/components/SearchBar/SearchBar.js
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import { search } from "../../store/actions/quote";
+import { typeahead } from "../../store/actions/typeahead";
+import { getAuthor } from "../../store/actions/author";
+import Results from "./Results";
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+  grid-area: searchbar;
+`;
+
+const Search = styled.input`
+  outline: none;
+  width: 100%;
+  box-sizing: border-box;
+  border: 2px solid #b5838d;
+  border-radius: 100px;
+  padding: 10px;
+  font-weight: bold;
+
+  &:placeholder-shown {
+    color: #6d6875;
+  }
+`;
+
+const Button = styled.button`
+  cursor: pointer;
+  background-color: #ffb4a2;
+  margin: 10px;
+  padding: 10px;
+  font-size: 1.2rem;
+  font-weight: bold;
+  border: 2px solid #6d6875;
+  border-radius: 50px;
+  transition: background-color 0.3s;
+
+  &:hover,
+  &:focus {
+    background-color: #e5989b;
+  }
+`;
+
+const SearchBar = ({ typeahead, author, getAuthors, getAuthor, search }) => {
+  const [query, setQuery] = useState("");
+  const [quotesNumber, setQuotesNumber] = useState(0);
+  const [showResults, setShowResults] = useState(false);
+
+  const onChangeHandler = e => {
+    setQuery(e.target.value);
+    setShowResults(true);
+    if (!typeahead.loading && !typeahead.success) {
+      getAuthors();
+    }
+  };
+
+  const onAuthorSelect = (authorName, authorQuotes) => {
+    setQuery(authorName);
+    setQuotesNumber(authorQuotes);
+    setShowResults(false);
+    search(authorName, authorQuotes);
+    getAuthor(authorName);
+  };
+
+  const getAnotherQuote = () => {
+    search(query, quotesNumber);
+  };
+
+  return (
+    <Container>
+      <Search
+        type="text"
+        placeholder="Type author's name: William Shakespeare"
+        value={query}
+        onFocus={() => setShowResults(true)}
+        onClick={() => setShowResults(true)}
+        onChange={onChangeHandler}
+        onKeyDown={e => {
+          if (typeahead.authors.length > 0 && e.which === 40) {
+            const result = document.querySelector('li[tabindex="0"]');
+            result ? result.focus() : null;
+          } else if (typeahead.authors.length > 0 && e.which === 38) {
+            const result = document.querySelector(
+              `li[tabindex="${document.querySelectorAll("li[tabindex]").length -
+                1}"]`
+            );
+            result ? result.focus() : null;
+          } else if (e.which === 27) {
+            setShowResults(false);
+          }
+        }}
+      />
+      {query.trim().length > 0 && author.success ? (
+        <Button onClick={getAnotherQuote}>Another Quote</Button>
+      ) : null}
+      <Results
+        authorSelected={onAuthorSelect}
+        show={showResults}
+        authors={typeahead.authors}
+        query={query}
+        loading={typeahead.loading}
+      />
+    </Container>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    typeahead: state.typeahead,
+    author: state.author
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getAuthors: () => dispatch(typeahead()),
+    getAuthor: name => dispatch(getAuthor(name)),
+    search: (author, quotes) => dispatch(search(author, quotes))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SearchBar);
